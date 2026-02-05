63e971b8c0dedb34a661a97311691c2c57f6bc59
unknown <1013362487@qq.com>
2026-02-05 16:49:18 +0800

    Update build with correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: '/smart-croop/',
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
