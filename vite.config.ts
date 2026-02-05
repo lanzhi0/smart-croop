360be0b0658b63580381bf6e0323416c04baaf37
unknown <1013362487@qq.com>
2026-02-05 18:39:13 +0800

    Fix image path and build files

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: '/smart-croop/',
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
